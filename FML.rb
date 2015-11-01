2834e173b201bac92d717be5a3bbf274f864009e
hyfr <japhetmperez@gmail.com>
2015-10-31 18:02:47 -0700

    userID's now work

--- FML.rb
+++ FML.rb
@@ -3,17 +3,9 @@ require 'sequel'
 DB = Sequel.sqlite('TheFML.db') #relative memory database
 
 class User
-  attr_reader :account_id
-
-  def userAccID
-    @account_id = DB[:user].where(:name=>euser, :password=>epassword).get(:user_id)
-  end
-  
-  def initialize()
-  end
 
   #Checks to see if a 'user' table exists in the FML database
-  def create
+  def verifyDB
     exist = DB.table_exists?(:user)
     if exist == true
       puts "There is a table named 'user' that exists. You have access to this table."
@@ -24,129 +16,176 @@ class User
         String :password
       end
     end
-    $userAccount = DB[:user] # Create a global dataset of the 'user' table
+    @@userAccount = DB[:user] # Creates a class variable dataset of the 'user' table.
   end
 
-  def userInput
+  def verifyAccount
     puts "Do you already have an account in the user database? "
     answer = gets.chomp.downcase
 
     if answer == "yes"
       puts "That is great. What is your username? "
-      eusername = gets.chomp.downcase
+      @@eusername = gets.chomp.downcase
       puts "And your password? "
-      epassword = gets.chomp.downcase
+      @@epassword = gets.chomp.downcase
 
-      if $userAccount.where(:name=>eusername).where(:password=>epassword).count
+      if @@userAccount.where(:name=>@@eusername).where(:password=>@@epassword).count > 0
         puts "Account exists."
+        @@userID = @@userAccount.where(:name => @@eusername).where(:password => @@epassword).get(:user_id)
 
         puts "Would you like to delete your account?"
+        
         danswer = gets.chomp.downcase
 
         if danswer == "yes"
-          $userAccount.delete()
+          @@userAccount.delete()
+          puts "Your account was deleted."
         else
           puts "Glad to have you stay with us."
         end
       else
         puts "The username or password is wrong."
         puts "Please try again."
-        exit
-      end
-      
+        def tryAgain
+          i = 0
+          while i <= 3 do
+            puts "You will be temporary kicked off if your password or username is incorrect three times."
+            puts "What is your username?"
+            username = gets.chomp.downcase
+            puts "What is your password?"
+            password = gets.chomp.downcase
 
-    # code some lines that check if the username and password exist in the DB.
-    else if answer == "no"
-      puts "What will be your username? "
+            if @@userAccount.where(:name => username).where(:password => password).count == 1
+              puts "Glad to have you with us."
+            else
+              puts "Username or password is wrong."
+              i += 1
+            end
+          end
+        end
+      end
+    elsif answer == "no"
+      puts "You should consider joining."
+    else
+      puts "Type either 'yes' or 'no'."
+    end
+  end
+
+  
+=begin
+  def tryAgain
+    i = 0
+    while i <= 3 do
+      puts "You will be temporary kicked off if your password or username is incorrect three times."
+      puts "What is your username?"
       username = gets.chomp.downcase
-      puts "What will be your password? "
+      puts "What is your password?"
       password = gets.chomp.downcase
 
-      #Fill table with user input based on user input
-      $userAccount.insert(:name => username, :password => password)
-    else puts "Answer 'yes' or 'no'"
+      if @@userAccount.where(:name => username).where(:password => password).count == 1
+        puts "Glad to have you with us."
+      else
+        puts "Username or password is wrong."
+        i += 1
+      end
     end
+  end
+=end
+      
+    
+  def createUser
+    puts "Since you do not have an account with us, would like to create an account?"
+    answer = gets.chomp.downcase
+    if answer == "yes"
+      puts "What will be your username?"
+      username = gets.chomp.downcase
+      puts "What will be your password?"
+      password = gets.chomp.downcase
 
+      @@userAccount.insert(:name => username, :password => password)
 
+    elsif answer == "no"
+      puts "That's too bad. We'd like to have you join us."
+    else
+      puts "Type either 'yes' or no."
+    end
+  end
+
+  def delAccount
     puts "Would you like to delete your account within the user database? "
     answer = gets.chomp.downcase
 
     if answer == "yes"
-      $userAccount.delete()
+      userDataSet.delete()
       puts "Your account was successfully deleted"
-    else
+    elsif answer == "no"
       puts "Glad to have you stay with us"
+    else
+      puts "Type either 'yes' or 'no'."
     end
   end
 end
-end
 
-class Forum
-  def initialize
-  end
 
-  def checkOrCreate
+class Post < User
+
+  def checkPostTable
     forumn = DB.table_exists?(:forum)
     if forumn == true
       puts "A forumn exists. You can post content onto this forumn."
     else
       DB.create_table :forum do
         primary_key :post_id
-        many_to_one :user, :key=>:user_id
         foreign_key :user_id, :user
         String :title
         String :content
       end
     end
-    $currentPost = DB[:forum] # Create a global dataset for the forumn table
+    @@currentPost = DB[:forum] # Creates a class variable from the forum table.
   end
 
-  def createPost(user)
+  def createPost(accountID)
+    
     puts "Would you like to create a post? "
-    posta = gets.chomp.downcase
+    answer = gets.chomp.downcase
 
-    if posta == "yes"
+    if answer == "yes"
       puts "What would you like the title of your post to be? "
       title = gets.chomp
 
       puts "What would you like the content of your post to be? "
       content = gets.chomp
-
-      #content_id = currentUser.userAccID
       
       #Fill the forumn table with a title and content
-      #$currentPost.insert(:user_id => content_id, :title => title, :content => content)
-
-      puts "Would you like to delete your post?"
-      dpost = gets.chomp.downcase
-
-      if dpost == "yes"
-        puts "Deleting your post."
-        $currentPost.delete()
-      else if dpost == "no"
-        puts "Understood, we will not delete your post."
-      else
-        puts "Type either 'yes' or 'no'."
-      end
-    end
-
-    else if posta == "no"
-      "Okay, we will not delete your post. As of now there is nothing left o do. The program will exit."
-
+      @@currentPost.insert(:user_id => @@userID, :title => title, :content => content)
+    elsif answer == "no"
+      puts "You should consider joining us."
     else
-      puts "Print either 'yes' or 'no'."
+      puts "Type either 'yes' or 'no'."
+    end
+  end
+
+  def delPost
+    puts "Would you like to delete your post?"
+    answer == gets.chomp.downcase
+
+    if answer == "yes"
+      puts "Deleting your post."
+      @@currentPost.delete()
+    elsif answer == no
+      puts "Understood, we will not delete your post."
+    else
+      puts "Type in either 'yes' or 'no'."
+    end
   end
 end
-end
-end
-  
 
 currentUser = User.new()
-currentUser.create
-currentUser.userInput
+currentUser.verifyDB
+currentUser.verifyAccount
+currentUser.createUser
+currentUser.delAccount
 
-currentPost = Forum.new()
-currentPost.checkOrCreate
-currentPost.createPost(currentUser)
-
-#forumn.method(database)
+currentPost = Post.new()
+currentPost.checkPostTable
+currentPost.createPost(currentUser.verifyAccount)
