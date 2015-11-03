ae09274cad2d99d40dcb3eca80b9700bf5c266cb
hyfr <japhetmperez@gmail.com>
2015-11-02 21:50:20 -0800

    Haven't turned this in yet

--- FML.rb
+++ FML.rb
@@ -27,42 +27,30 @@ class User
       puts "That is great. What is your username? "
       @@eusername = gets.chomp.downcase
       puts "And your password? "
-      @@epassword = gets.chomp.downcase
+      @@epassword = gets.chomp.downcase 
 
+      # user input gets stored into a class instance variable and stored into DB
       if @@userAccount.where(:name=>@@eusername).where(:password=>@@epassword).count > 0
         puts "Account exists."
         @@userID = @@userAccount.where(:name => @@eusername).where(:password => @@epassword).get(:user_id)
 
         puts "Would you like to delete your account?"
-        
         danswer = gets.chomp.downcase
 
+        puts "What is your username?"
+        dusername = gets.chomp
+
         if danswer == "yes"
-          @@userAccount.delete()
+          @@userAccount.where(:name => dusername).delete()
           puts "Your account was deleted."
-        else
+        elsif danswer == "no"
           puts "Glad to have you stay with us."
+        else
+          puts "Type in either 'yes' or 'no.'"
         end
       else
         puts "The username or password is wrong."
         puts "Please try again."
-        def tryAgain
-          i = 0
-          while i <= 3 do
-            puts "You will be temporary kicked off if your password or username is incorrect three times."
-            puts "What is your username?"
-            username = gets.chomp.downcase
-            puts "What is your password?"
-            password = gets.chomp.downcase
-
-            if @@userAccount.where(:name => username).where(:password => password).count == 1
-              puts "Glad to have you with us."
-            else
-              puts "Username or password is wrong."
-              i += 1
-            end
-          end
-        end
       end
     elsif answer == "no"
       puts "You should consider joining."
@@ -71,38 +59,36 @@ class User
     end
   end
 
-  
-=begin
   def tryAgain
     i = 0
-    while i <= 3 do
-      puts "You will be temporary kicked off if your password or username is incorrect three times."
+    puts "You will be temporary kicked off if your password or username is incorrect three times."
+    while i < 3 do
       puts "What is your username?"
-      username = gets.chomp.downcase
+      @username = gets.chomp.downcase
       puts "What is your password?"
-      password = gets.chomp.downcase
+      @password = gets.chomp.downcase
 
-      if @@userAccount.where(:name => username).where(:password => password).count == 1
+      # The .count method returns a number and if the number is > 0, the int signifies the number of accounts that exist with the specific @username and @password. if int returned from .count > 0 then an account exists.
+      if @@userAccount.where(:name => @username).where(:password => @password).count > 0
         puts "Glad to have you with us."
+        i = 3
       else
         puts "Username or password is wrong."
         i += 1
       end
     end
   end
-=end
-      
-    
+  
   def createUser
     puts "Since you do not have an account with us, would like to create an account?"
     answer = gets.chomp.downcase
     if answer == "yes"
       puts "What will be your username?"
-      username = gets.chomp.downcase
+      @@eusername = gets.chomp.downcase
       puts "What will be your password?"
-      password = gets.chomp.downcase
+      @@epassword = gets.chomp.downcase
 
-      @@userAccount.insert(:name => username, :password => password)
+      @@userAccount.insert(:name => @@eusername, :password => @@epassword)
 
     elsif answer == "no"
       puts "That's too bad. We'd like to have you join us."
@@ -116,7 +102,7 @@ class User
     answer = gets.chomp.downcase
 
     if answer == "yes"
-      userDataSet.delete()
+      @@userAccount.where(:name => @@eusername).where(:password => @@epassword).delete()
       puts "Your account was successfully deleted"
     elsif answer == "no"
       puts "Glad to have you stay with us"
@@ -125,7 +111,6 @@ class User
     end
   end
 end
-
 
 class Post < User
 
@@ -141,23 +126,22 @@ class Post < User
         String :content
       end
     end
-    @@currentPost = DB[:forum] # Creates a class variable from the forum table.
+    @@currentPost = DB[:forum] # Creates a class variable from the forum table that holds a Post dataset.
   end
 
   def createPost(accountID)
-    
     puts "Would you like to create a post? "
     answer = gets.chomp.downcase
 
     if answer == "yes"
       puts "What would you like the title of your post to be? "
-      title = gets.chomp
+      @@title = gets.chomp
 
       puts "What would you like the content of your post to be? "
-      content = gets.chomp
+      @@content = gets.chomp
       
       #Fill the forumn table with a title and content
-      @@currentPost.insert(:user_id => @@userID, :title => title, :content => content)
+      @@currentPost.insert(:user_id => @@userID, :title => @@title, :content => @@content)
     elsif answer == "no"
       puts "You should consider joining us."
     else
@@ -166,13 +150,17 @@ class Post < User
   end
 
   def delPost
-    puts "Would you like to delete your post?"
-    answer == gets.chomp.downcase
+    puts "#{@@currentPost.where(:user_id => @@userID).all}"
+    puts "Your account's posts have been displayed. Type the Title of the post you want to delete."
+    title = gets.chomp
 
-    if answer == "yes"
+    puts "LAST WARNING: Would you like to delete this post?"
+    deletion = gets.chomp.downcase
+
+    if deletion == "yes"
       puts "Deleting your post."
-      @@currentPost.delete()
-    elsif answer == no
+      @@currentPost.where(:title => title).delete()
+    elsif deletion == "no"
       puts "Understood, we will not delete your post."
     else
       puts "Type in either 'yes' or 'no'."
@@ -180,12 +168,16 @@ class Post < User
   end
 end
 
+# Create an instance of User class and verify that every method works.
 currentUser = User.new()
 currentUser.verifyDB
 currentUser.verifyAccount
 currentUser.createUser
+currentUser.tryAgain
 currentUser.delAccount
 
+# Create an isntance of Post class and verify that every method works
 currentPost = Post.new()
 currentPost.checkPostTable
-currentPost.createPost(currentUser.verifyAccount)
+currentPost.createPost(currentUser.verifyAccount) 
+currentPost.delPost
